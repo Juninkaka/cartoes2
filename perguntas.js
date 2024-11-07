1f5432f6b5eaad0c15d4d2bbcfe6de3d6a41479a
Junin <131893553+Juninkaka@users.noreply.github.com>
2024-11-07 09:32:53 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+CriaCartao (
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+CriaCartao (
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+CriaCartao (
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+CriaCartao (
+    'Lingua Inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em inglês é HI (RAI)'
+)
